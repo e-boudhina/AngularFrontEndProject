575b2ac9a4aff6ee3f657d2f74a124c7b8f0c0f2
Elyes Boudhina <elyes.boudhina@etudiant-fsegt.utm.tn>
2021-12-11 17:43:42 +0100

    Removing errors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,37 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LayoutComponent } from './home/layout/layout.component';
+import {ListStockComponent} from "./management/stock-managment/list-stock/list-stock.component";
+import {AddStockComponent} from "./management/stock-managment/add-stock/add-stock.component";
+import {UpdateStockComponent} from "./management/stock-managment/update-stock/update-stock.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {ListRayonComponent} from "./management/rayon-management/list-rayon/list-rayon.component";
+import {AddRayonComponent} from "./management/rayon-management/add-rayon/add-rayon.component";
+import {UpdateRayonComponent} from "./management/rayon-management/update-rayon/update-rayon.component";
+
 
 const routes: Routes = [
   {
-    path: 'home', component: HomeComponent, children: [
-      {path: '', component: LayoutComponent},]
-  }
+    path: '', component: HomeComponent,
+    children: [
+        {  path: 'stock-management',
+                      children: [
+                                        { path: 'list-stock', component: ListStockComponent },
+                                        { path: 'add-stock', component: AddStockComponent },
+                                        { path: 'update-stock/:id', component: UpdateStockComponent }
+                                      ]
+        },
+      { path: 'rayon-management',
+                      children:[
+                                        {path: 'list-rayon', component: ListRayonComponent},
+                                        {path: 'add-rayon', component: AddRayonComponent},
+                                        {path: 'update-rayon/:id', component: UpdateRayonComponent}
+                                      ]
+      }
+    ]
+  },
+  { path: '**', component:  PageNotFoundComponent}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -8,7 +8,18 @@ import { FooterComponent } from './home/footer/footer.component';
 import { NavbarComponent } from './home/navbar/navbar.component';
 import { SidbarComponent } from './home/sidbar/sidbar.component';
 import { LayoutComponent } from './home/layout/layout.component';
-
+import {HttpClientModule} from "@angular/common/http";
+import {ListStockComponent} from "./management/stock-managment/list-stock/list-stock.component";
+import {AddStockComponent} from "./management/stock-managment/add-stock/add-stock.component";
+import {ReactiveFormsModule} from "@angular/forms";
+import { UpdateStockComponent } from './management/stock-managment/update-stock/update-stock.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ListRayonComponent } from './management/rayon-management/list-rayon/list-rayon.component';
+import { AddRayonComponent } from './management/rayon-management/add-rayon/add-rayon.component';
+import { UpdateRayonComponent } from './management/rayon-management/update-rayon/update-rayon.component';
+import {NgxPaginationModule} from 'ngx-pagination';
+import {Ng2SearchPipeModule} from "ng2-search-filter";
+import { ErrorsComponent } from './errors/errors/errors.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,11 +27,26 @@ import { LayoutComponent } from './home/layout/layout.component';
     FooterComponent,
     NavbarComponent,
     SidbarComponent,
-    LayoutComponent
+    LayoutComponent,
+    ListStockComponent,
+    AddStockComponent,
+    UpdateStockComponent,
+    PageNotFoundComponent,
+    ListRayonComponent,
+    AddRayonComponent,
+    UpdateRayonComponent,
+    ErrorsComponent,
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    NgxPaginationModule,
+    Ng2SearchPipeModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
